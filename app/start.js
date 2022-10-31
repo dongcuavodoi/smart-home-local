81e11af42a80d0d30379090d1f9c5e4e0f254b89
Nguyen Ba Luan <61571929+dongcuavodoi@users.noreply.github.com>
2022-10-31 11:42:21 +0700

    update

---
+++ app/start.js
@@ -0,0 +1,46 @@
+var udp = require("dgram");
+var PORT = 8888 ;
+var HOST = '192.168.1.56';
+
+var server = udp.createSocket("udp4");
+server.on("error", function (error) {
+  console.log("Error: " + error);
+  server.close();
+});
+server.on("message", function (msg, info) {
+  console.log("Data received from client : " + msg.toString());
+  console.log(
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port
+  );
+  server.send(msg, info.port, "localhost", function (error) {
+    if (error) {
+      client.close();
+    } else {
+      console.log("Data sent !!!");
+    }
+  });
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log("Server is listening at port " + port);
+  console.log("Server ip : " + ipaddr);
+  console.log("Server is IP4/IP6 : " + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on("close", function () {
+  console.log("Socket is closed !");
+});
+
+server.bind(8112);
+
+setTimeout(function(){
+server.close();
+},60000);
